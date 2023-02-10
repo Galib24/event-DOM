4a134940f7c2641fe78cc320c0e84089c81fe4a4
SQLAdmin <abuyeahia24@gmail.com>
2023-02-10 12:28:27 +0600

    comment-box added

--- js/event.js
+++ js/event.js
@@ -3,12 +3,13 @@ console.log('This is separate JS file');
 // directly set on the option
 
 // option 2
+// <button onclick="makeRed()">Make Red</button>  we will use this most
 function makeRed(){
     document.body.style.backgroundColor = 'red';
 }
 
 
- // option 3
+ // option 3 use some time
  const makeBlueButton = document.getElementById('make-blue');
  makeBlueButton.onclick = makeBlue;
 
@@ -21,3 +22,15 @@ function makeRed(){
  purpleButton.onclick =function makePurple(){
      document.body.style.backgroundColor = 'purple';
  }
+//  pink
+const pinkButton = document.getElementById('make-pink');
+pinkButton.addEventListener('click', makePink);
+
+function makePink(){
+    document.body.style.backgroundColor = 'pink';
+}
+
+// golden-rod this option we use most
+document.getElementById('make-goldenrod').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'goldenrod'
+  })
